32b390db6650c9d35507078c7b3c2c2aaf53ae52
Adrimi <adriano100417@gmail.com>
2020-09-15 13:18:51 +0200

    geneaated project with Apollo dependency

---
+++ GithubExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubExplorer
+//
+//  Created by adrian.szymanowski on 15/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
